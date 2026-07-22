e1676711b508e12f7f2ce70119d0c494664fb84c
Shubham Chawla <shubhamchawla200307@gmail.com>
2026-07-23 02:53:22 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (6.11%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -27,7 +27,7 @@ public:
     }*/
     
     //using dp
-    int fun(vector<int>& a,int n,int i,int free,vector<vector<int>>& dp){
+   /* int fun(vector<int>& a,int n,int i,int free,vector<vector<int>>& dp){
 
         if(i==n) return 0;
 
@@ -58,5 +58,32 @@ public:
 
         return fun(nums,n,0,1,dp);
         
-    }
+    }*/
+
+    int fun(vector<int>& nums,int n,int i,vector<int>& dp){
+    if(i==n-1) return nums[n-1];
+
+    if(i>=n) return 0;
+
+    if(dp[i] != -1) return dp[i];
+
+    int lena_hai = fun(nums,n,i+2,dp) + nums[i];
+
+    int nhi_lena_hai = fun(nums,n,i+1,dp);
+
+    int ans = max(lena_hai,nhi_lena_hai);
+
+    dp[i] = ans;
+
+    return ans;
+}
+int rob(vector<int> &nums){
+    // Write your code here.
+
+    int n = nums.size();
+
+    vector<int> dp(n+1,-1);
+
+    return fun(nums,n,0,dp);
+}
 };
